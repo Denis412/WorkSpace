2914536d9b21d40c4faafe01afd67418678968b0
Denis412 <d-a-malyshev@yandex,ru>
2023-03-29 16:41:06 +0300

    Удаление ненужных компонентов, организация структуры компонента MainLayout, добавлены ENV переменные с ссылками на API

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      />
+
+      <q-toolbar-title>{{ title }}</q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup>
+const { toggleLeftDrawer, title } = defineProps({
+  toggleLeftDrawer: Function,
+  title: String,
+});
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,32 +1,30 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh Lpr fFf">
+    <MainHeader :toggleLeftDrawer="toggleLeftDrawer" title="Space" />
 
-        <q-toolbar-title></q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list> </q-list>
-    </q-drawer>
+    <MainDrawer v-model="leftDrawerOpen" side="left" title="Список">
+      <template #list>
+        <!-- Место для контентента -->
+      </template>
+    </MainDrawer>
 
     <q-page-container>
-      <router-view />
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </q-page-container>
+
+    <MainFooter />
   </q-layout>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import MainHeader from "src/components/MainHeader.vue";
+import MainDrawer from "src/components/MainDrawer.vue";
+import MainFooter from "src/components/MainFooter.vue";
 
 const leftDrawerOpen = ref(false);
 
