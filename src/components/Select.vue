d34b4ebf2770ab79839a9af94f58375b36e1d34f
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-04-11 12:59:43 +0300

    Создание универсального компонента селекта

---
+++ src/components/Select.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-select
+      filled
+      v-model="changeBy"
+      :options="options"
+      use-chips
+      stack-label
+      label="Группировка"
+      @update:model-value="change"
+
+    />
+</template>
+
+<script setup>
+import { defineEmits,defineProps,ref } from "vue";
+
+const emit = defineEmits(["change"]);
+const { options, changeValue } = defineProps({
+  options:Array,
+  changeValue:String
+})
+
+const changeBy = ref(changeValue);
+
+const change =()=>{
+  emit("change",changeBy.value)
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
